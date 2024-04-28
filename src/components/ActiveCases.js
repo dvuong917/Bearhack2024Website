e8cdce94ec71c487e7f5f2689cc5baa4d3b2b6d7
dvuong917 <dvuongwm@gmail.com>
2024-04-28 05:37:44 -0700

    covid

--- src/components/ActiveCases.js
+++ src/components/ActiveCases.js
@@ -2,17 +2,17 @@
 
 import { useState, useEffect } from "react";
 
-export default function Home({ state }) {
+const ActiveCases = ({ state }) => {
   const [showPosts, setshowPosts] = useState();
 
-  const apiUrl = "https://disease.sh/v3/covid-19/states/{state}";
+  const apiUrl = `https://disease.sh/v3/covid-19/states/${state}`;
 
   let displayData;
 
   async function pullJson() {
     const response = await fetch(apiUrl);
     const responseData = await response.json();
-    displayData = responseData.cases;
+    displayData = responseData.active;
     setshowPosts(displayData);
     console.log(responseData);
   }
@@ -21,13 +21,7 @@ export default function Home({ state }) {
     pullJson();
   }, []);
 
-  return (
-    <div className="">
-      <main className="">
-        <h1>
-          Cases of COVID in {state}: {showPosts}
-        </h1>
-      </main>
-    </div>
-  );
-}
+  return showPosts;
+};
+
+export default ActiveCases;
