df69ac3dbacb5e3fef78d585c3a0842b826b2005
TheTaiko <normandyng7@gmail.com>
2024-04-27 18:05:09 -0700

    Worked on formating on page 2

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,17 +2,23 @@ import React from "react";
 
 const Results = () => {
   return (
-    <div className="flex justify-center items-center bg-sky-800">
-      <div className="text-6xl text-sky-400">Search Results</div>
-      <div className="text-base text-neutral-950">Sort By: </div>
-      <div className="flex flex-row p-2 round-lg border border-solid border-black">
-        Profile Picture
+    <div className="justify-center items-center bg-sky-800">
+      <div className="flex flex-col text-6xl text-sky-400 font-bold">
+        Search Results
       </div>
-      <div className="flex flex-row p-2 round-lg border border-solid border-black">
-        <div>Name: </div>
-        <div>Location: </div>
-        <div>NPI: </div>
-        <div>Taxonomy: </div>
+      <div className="text-base text-neutral-950">Sort By: </div>
+      <div className="ml-15">
+        <div className="flex flex-row p-2 round-lg border border-solid border-black">
+          Profile Picture
+        </div>
+      </div>
+      <div className="ml-15">
+        <div className="flex flex-row p-2 round-lg border border-solid border-black">
+          <div>Name: </div>
+          <div>Location: </div>
+          <div>NPI: </div>
+          <div>Taxonomy: </div>
+        </div>
       </div>
       <div className="flex flex-row p-2 round-lg border border-solid border-black">
         Profile Picture
